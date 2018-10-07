e3546ebc8662d396280ff65daac3399f67a04207
fediuchkoelvira <fediuchko.elvira@gmail.com>
2018-10-07 15:10:45 +0300

    added search  functionality

--- src/components/input/index.jsx
+++ src/components/input/index.jsx
@@ -1,57 +1,19 @@
 import React from "react";
 import { connect } from "react-redux";
 import { mapStateToProps, mapDispatchToProps } from "./container";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import "./index.scss";
 
-// export class SearchInput extends React.Component {
-
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       value: '',
-//       posts: []
-//     };
-//   }
-//   componentDidMount() {
-//     console.log("componentDidMount" + JSON.stringify(this.props))
-//   //this.props.getAllPosts()
-//   }
-
-//   handleGetValue(e) {
-//     this.setState(
-//       {
-//         value: e.target.value
-//       },
-//       console.log(this.state.value)
-//     );
-//     this.props.onQueryChange(this.state.value);
-//   }
-
-//   render() {
-
-//     return (
-//       <div>
-//         <input
-//           type="search"
-//           placeholder="Пoшук"
-//           onChange={this.handleGetValue}
-//         />
-//         <Blog posts={this.state.posts} />
-//         </div>
-//     )
-//   }
-// }
 
 export class SearchInput extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: "",
+      query: "",
       posts: []
     };
   }
-
-  componentWillMount() {
+  getInfo = () => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then(response => response.json())
       .then(posts => {
@@ -59,54 +21,73 @@ export class SearchInput extends React.Component {
           posts: posts
         });
       });
+  };
+  componentWillMount() {
+    this.getInfo();
   }
 
-  handleGetValue = e => {
-    this.setState(
-      {
-        value: e.target.value
-      },
-      console.log(this.state.value)
-    );
-    this.props.onQueryChange(this.state.value);
+  handleInputChange = event => {
+    let updatedList = this.state.posts;
+    updatedList = updatedList.filter(function(post) {
+      return post.title
+        .toLowerCase()
+        .includes(event.target.value.toLowerCase())||post.body.toLowerCase()
+        .includes(event.target.value.toLowerCase())
+    });
+    this.setState({ posts: updatedList });
   };
   handleCheck = e => {
-    console.log("handleCheck")
+    console.log("handleCheck");
     alert(e);
   };
   render() {
     return (
-      <span>
-        <div class="header" style={{
-      
-        }} >
-  <h2>My Blog</h2>
-</div>
-      <div class="row">
-        <div class="card">
-        <h5>Search</h5>
-        <input
-          type="search"
-          placeholder="Пoшук"
-          // onChange={this.handleGetValue}
-          />
+      <div class="wrap">
+        <header class="top-header">
+          <div class="logo">
+            <img
+              src="https://jimenogray-dev.fosterwebmarketing.com/includes/assets/images/logos/logo-dark.png"
+              alt=""
+            />
+          </div>
+          <nav />
+        </header>
+        <div class="content">
+          <aside>
+            <div class="sidebar-header">
+              <h1>Blog</h1>
+              <div class="summary" />
+            </div>
+          </aside>
+          <main>
+            <div class="search">
+              <input
+                onChange={this.handleInputChange}
+                class="form-control"
+                type="search"
+                placeholder="Search Blogs"
+              />
+              <i class="fa fa-search" />
+            </div>
+            <ul class="content-list">
+              {this.state.posts.map(item => (
+                <li class="content-item" key={item.id}>
+                  <span class="details">
+                    <Link to={`/${item.id}`}>
+                      <h2 class="title"> {item.title}</h2>
+                    </Link>
+                    <span class="summary"> {item.body}</span>
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </main>
         </div>
-        <div class="card">
-        <ul>
-          {this.state.posts.map(item => (
-            <li
-            key={item.id}>
-              <Link to={`/${item.id}`}>
-                  {item.title}
-                </Link>
-            </li>
-          ))}
-        </ul>
-        </div>
-        </div>
-        </span>
+        <footer>
+          <small />
+        </footer>
+      </div>
     );
-   
   }
 }
 export default connect(
--- src/components/postPage/index.jsx
+++ src/components/postPage/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import "./index.scss";
 
 export class PostPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       post: null,
-        comments: [],
-      user: {name:"",username:""}
+      comments: [],
+      user: { name: "", username: "" }
     };
   }
   componentWillMount() {
@@ -19,14 +20,14 @@ export class PostPage extends React.Component {
         this.setState({
           post: post
         });
-        return fetch("https://jsonplaceholder.typicode.com/users/" + post.userId)
-            .then(response => response.json())
-            .then(user => {
-                console.log("user"+ JSON.stringify(user))
-                this.setState({
-              
-
-                user: user
+        return fetch(
+          "https://jsonplaceholder.typicode.com/users/" + post.userId
+        )
+          .then(response => response.json())
+          .then(user => {
+            console.log("user" + JSON.stringify(user));
+            this.setState({
+              user: user
             });
             return fetch(
               "https://jsonplaceholder.typicode.com/comments?postId=" +
@@ -46,17 +47,36 @@ export class PostPage extends React.Component {
     if (!this.state.post) return <div>Загрузка...</div>;
     return (
       <div>
-        <h2>{this.state.post.title}</h2>
+        <div class="article">
+          <div class="title">
+            <h2>{this.state.post.title}</h2>
+          </div>
+          <div class="content">
             <div>{this.state.post.body}</div>
-            <div><h5>Name:</h5>{this.state.user.name} <h5> Username:</h5>{this.state.user.username}</div>
-        <ul>
-          {this.state.comments.map(comment => (
-            <li key={comment.id}>
-              <h5> {comment.name}</h5>
-              {comment.body}
-            </li>
-          ))}
-        </ul>
+            <p class="small">
+              <div>
+                <h5>Name:</h5>
+                {this.state.user.name} <h5> Username:</h5>
+                {this.state.user.username}
+              </div>
+            </p>
+          </div>
+        </div>
+        <div class="article-2">
+          <div class="title">
+            <h2>Comments</h2>
+          </div>
+          <div class="glass">
+            <ul>
+              {this.state.comments.map(comment => (
+                <li key={comment.id}>
+                  <h5> {comment.name}</h5>
+                  {comment.body}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
